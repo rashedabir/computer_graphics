128794ff656e6ea0d77a3f10526090cf521dbfbd
rashedabir <rashed15-2155@diu.edu.bd>
2021-10-16 11:11:49 +0600

    DDA Algorithm Practice

--- Straight Line/main.cpp
+++ Straight Line/main.cpp
@@ -1,94 +1,78 @@
-#include<stdio.h>
-#include <GL/gl.h>
 #include <GL/glut.h>
-float x1,y1,x2,y2,m,i,j;
-float dx,dy;
-void display(void)
+#include <stdio.h>
+#include <GL/gl.h>
+#include <math.h>
+
+void init(void)
 {
-    /* clear all pixels */
-    glClear (GL_COLOR_BUFFER_BIT);
-    /* draw white polygon (rectangle) with corners at
-    * (0.25, 0.25, 0.0) and (0.75, 0.75, 0.0)
-    */
-    glEnd();
-
-    glColor3f (0.0, 1.0, 0.0);
-    glBegin(GL_POINTS);
-
-    if(m>0 && m<=1)
-    {
-        while(x1<=x2 && y1<=y2)
-        {
-            x1=x1+1;
-            y1=y1+m;
-            glVertex3f(x1/100,y1/100,0.0);
-            printf("%f %f",x1,y1);
-
-        }
-    }
-    else if(m>1)
-    {
-        while(x1<=x2 && y1<=y2)
-        {
-            x1=x1+(1/m);
-            y1=y1+1;
-            glVertex2d(x1/100,y1/100);
-            printf("%f %f",x1,y1);
-        }
-    }
-
-    else if(m>-1 && m<=0)
-    {
-        while(x1>=x2 && y1>=y2)
-        {
-            x1=x1-1;
-            y1=y1-m;
-            glVertex3f(x1/100,y1/100,0.0);
-            printf("%f %f",x1,y1);
-        }
-    }
-    else if(m<-1)
-
-    {
-
-        while(x1>=x2 && y1>=y2)
-        {
-            x1=x1-(1/m);
-            y1=y1-1;
-            glVertex3f(x1/100,y1/100,0.0);
-            printf("%f %f",x1,y1);
-        }
-    }
-
-    glEnd();
-
-    glFlush ();
-}
-void init (void)
-{
-    glClearColor (0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
+    glClearColor(0.0,0.0,0.0,0.0); //GLfloat red,green,blue,alpha initial value 0 alpha values used by glclear to clear the color buffers
+    glMatrixMode(GL_PROJECTION);  // To specify which matrix is the current matrix & projection applies subsequent matrix to projecton matrix stack
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glOrtho(0.0, 300.0, 0.0, 300.0, -1.0, 1.0);
 }
 
-int main(int argc, char** argv)
+void Draw()
 {
-    x1 = 21;
-    y1 = 87;
-    x2 = 387;
-    y2 = 321;
-    dx=x2-x1;
-    dy=y2-y1;
-    m=dy/dx;
 
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("Straight Line Draw Using DDA Algorithm");
-    init ();
-    glutDisplayFunc(display);
+    int xs=21, ys=87, xe=387, ye=321;     //starting and ending point
+    //x1 is xs, x2 is xe, y1 is ys, y2 is ye
+    double xi, yi, m;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f( 1,0, 0);
+
+    m = (ye-ys)/(xe-xs);
+
+    if(xe>xs){
+        if(m<=1){
+            xi = 1;
+            yi = m;
+        }   //case 1
+        else if(m>1){
+            yi = 1;
+            xi = 1/m;
+        }
+    }       //case 1 or case 2
+    else if(xs>xe){
+        if(m<=1){
+            xi = -1;
+            yi = -m;
+        }   //case 3
+        else if(m>1){
+            xi = (-1)/m;
+            yi = -1;
+        }   //case 4
+    }       //case 3 or case 4
+
+    //here xi is change of x and yi is change of y
+
+    glBegin(GL_POINTS);
+    glVertex2i(round(xs), round(ys));
+
+    while(1){
+        xs = xs+xi;
+        ys = ys+yi;
+
+        glVertex2i(round(xs), round(ys));
+
+        if(xs==xe || ys==ye){
+            break;
+        }
+    }
+
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc,char **argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE );
+    glutInitWindowPosition(0,0);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Lab Final");
+    init();
+    glutDisplayFunc(Draw);
     glutMainLoop();
     return 0;
 }
